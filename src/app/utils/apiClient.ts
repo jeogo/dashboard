1e7b695e0373a93fc0594e74e46bebc54122fe67
jeogo <65985256+jeogo@users.noreply.github.com>
2025-03-12 22:54:20 +0100

    Update apiClient.ts

--- src/app/utils/apiClient.ts
+++ src/app/utils/apiClient.ts
@@ -1,7 +1,7 @@
 // src/utils/apiClient.ts
 
 const BASE_URL =
-  process.env.NEXT_PUBLIC_BASE_URL || "https://storebot-xfhu.onrender.com";
+  process.env.NEXT_PUBLIC_BASE_URL || "https://store-97g7mwukn-jeogos-projects-a72871fd.vercel.app";
 
 type ApiResponse<T = any> = {
   data?: T;
