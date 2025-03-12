c1e88a0860bf4dc27220ff8c50002bf97de2a858
jeogo <65985256+jeogo@users.noreply.github.com>
2025-03-13 00:05:23 +0100

    Update apiClient.ts

--- src/app/utils/apiClient.ts
+++ src/app/utils/apiClient.ts
@@ -1,7 +1,7 @@
 // src/utils/apiClient.ts
 
 const BASE_URL =
-  process.env.NEXT_PUBLIC_BASE_URL || "https://store-bot-iota.vercel.app";
+  process.env.NEXT_PUBLIC_BASE_URL || "https://storebot-im89.onrender.com";
 
 type ApiResponse<T = any> = {
   data?: T;
